82588ce73cb77601092e183f1ac718181f820de9
onion knight <1840802136@qq.com>
2017-06-11 20:33:52 +0800

    Add files via upload

---
+++ Fragment_Right.java
@@ -0,0 +1,21 @@
+package com.example.drawlayout_qq;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_Right extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_right,container, false);
+	}
+}
